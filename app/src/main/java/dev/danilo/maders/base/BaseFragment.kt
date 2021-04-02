d08a1902466a78faf69bb9eaf345cb60d69b6c42
danilo-jsouza <danilo.jsouza02@gmail.com>
2021-04-02 20:20:04 -0300

    handler logout

--- app/src/main/java/dev/danilo/maders/base/BaseFragment.kt
+++ app/src/main/java/dev/danilo/maders/base/BaseFragment.kt
@@ -1,14 +1,15 @@
 package dev.danilo.maders.base
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import dev.danilo.maders.R
 
 abstract class BaseFragment<T : ViewBinding> : Fragment() {
     private lateinit var binding: T
+    private var toolbar: Toolbar? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,5 +20,22 @@ abstract class BaseFragment<T : ViewBinding> : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        toolbar = activity?.findViewById<Toolbar?>(R.id.toolbar)
+        toolbar?.setTitle(getTitle())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(getMenu(), menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
     abstract fun getViewBinding(): T
+
+    abstract fun getTitle(): Int
+
+    abstract fun getMenu(): Int
+
 }
--- app/src/main/java/dev/danilo/maders/feature/products/fragment/ProductFragment.kt
+++ app/src/main/java/dev/danilo/maders/feature/products/fragment/ProductFragment.kt
@@ -1,8 +1,12 @@
 package dev.danilo.maders.feature.products.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
+import dev.danilo.maders.R
+import dev.danilo.maders.SplashActivity
 import dev.danilo.maders.base.BaseFragment
 import dev.danilo.maders.databinding.FragmentProductBinding
 import dev.danilo.maders.feature.login.viewModel.LoginViewModel
@@ -17,4 +21,24 @@ class ProductFragment : BaseFragment<FragmentProductBinding>() {
         super.onViewCreated(view, savedInstanceState)
 
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.search_product -> TODO()
+            R.id.exit_product -> logout()
+            R.id.settings_product -> TODO()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun logout() {
+        val intent = Intent(requireContext(), SplashActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        activity?.finish()
+    }
+
+    override fun getTitle(): Int = R.string.products
+
+    override fun getMenu(): Int = R.menu.products
 }
